2c32e530b4b495fb18680483f7b949d88f942451
Nino <nino@ninux.org>
2019-11-27 23:30:53 +0100

    serialize e deserialize in sequenza, da sisetemare il modo di accedere con i puntatori tra prima e dopo il deserialize

--- examples/get-started/hello_world/main/hello_world_main.c
+++ examples/get-started/hello_world/main/hello_world_main.c
@@ -52,31 +52,41 @@ int test4(){
   char* buffer;
   sensordata_serialize(&sensordata,(uint8_t**) &buffer);
   printf("mio buffer: %s\n",buffer);
-
-
   printf("lunghezza buf:%d\n",strlen((char*)buffer));
 
   Ninux__Sensordata *sensordata2;
-  //sensordata2 = ninux__sensordata__unpack(NULL,strlen(buffer),(uint8_t*) buffer);
-
-  //printf("n_sets:%d\n",sensordata2->n_sets);
- // sensordata_print_all(sensordata2);
 
   Ninux__Sensordata *psensordata2 = &sensordata2;
   sensordata_deserialize(psensordata2,(unsigned char*) buffer);
-  //sensordata_print_all(&sensordata2);
+
   Ninux__Myset *set3;
   set3= malloc (sizeof (Ninux__Myset)); //one insert at once
   sensordata_add_set(sensordata2,set3,555555);
   sensordata_add_entry(sensordata2,set3,"temp",55);
   sensordata_add_entry(sensordata2,set3,"pres",5555);
   sensordata_print_all(sensordata2);
-  //int i,k;
-  //for (i = 0; i < sensordata2->n_sets; i++){
-  //    for (k = 0; k < sensordata2->sets[i]->n_entries; k++){
-  //            printf ("%d %s %d\n", sensordata2->sets[i]->timestamp,sensordata2->sets[i]->entries[k]->key,sensordata2->sets[i]->entries[k]->value);
-  //    }
-  //} 
+
+
+
+  
+  char* buffer2;
+  sensordata_serialize(sensordata2,(uint8_t**) &buffer2);
+  printf("mio buffer: %s\n",buffer2);
+  printf("lunghezza buf:%d\n",strlen((char*)buffer2));
+
+  Ninux__Sensordata *sensordata3;
+
+  Ninux__Sensordata *psensordata3 = &sensordata3;
+  sensordata_deserialize(psensordata3,(unsigned char*) buffer2);
+
+  Ninux__Myset *set4;
+  set4= malloc (sizeof (Ninux__Myset)); //one insert at once
+  sensordata_add_set(sensordata3,set4,333333);
+  sensordata_add_entry(sensordata3,set4,"temp",33);
+  sensordata_add_entry(sensordata3,set4,"pres",3333);
+  sensordata_print_all(sensordata3);
+
+  
   //sensordata_free(&sensordata);
   //sensordata_free(&sensordata2);
   return 0;
