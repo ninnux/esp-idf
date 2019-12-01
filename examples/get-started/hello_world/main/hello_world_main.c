e5f7b9e18c6593a49fd22106adcf53deed410c08
Nino <nino@ninux.org>
2019-12-01 09:32:12 +0100

    gira

--- examples/get-started/hello_world/main/hello_world_main.c
+++ examples/get-started/hello_world/main/hello_world_main.c
@@ -18,97 +18,40 @@
 char rtc_buffer[1024];
 int rtc_buffer_len=0;
 
-//int insert_values(unsigned char** buffer){
-//
-//  /// inserimento singolo a rotazione
-//  Ninux__Sensordata *sensordata2;
-//  Ninux__Sensordata *psensordata2 = &sensordata2;
-//  sensordata_deserialize(psensordata2,(unsigned char*) *buffer);
-//  Ninux__Myset *set3;
-//  set3= malloc (sizeof (Ninux__Myset)); //one insert at once
-//  sensordata_add_set(sensordata2,set3,555555);
-//  sensordata_add_entry(sensordata2,set3,"temp",55);
-//  sensordata_add_entry(sensordata2,set3,"pres",5555);
-//  sensordata_add_entry(sensordata2,set3,"humi",55);
-//  sensordata_print_all(sensordata2);
-//  sensordata_serialize(sensordata2,(uint8_t**) buffer);
-//  printf("mio buffer: %s\n",*buffer);
-//  printf("lunghezza buf:%d\n",strlen((char*)*buffer));
-//  /////////////
-//  return 0;
-//}
-
-int insert_values2(unsigned char** buffer, int timestamp, char** keys,int* values, int len, int* size){
-
-  /// inserimento singolo a rotazione
-  Ninux__Sensordata *sensordata2;
-  sensordata2=malloc(sizeof(Ninux__Sensordata));
-  Ninux__Sensordata **psensordata2 = &sensordata2;
-  //sensordata_deserialize(psensordata2,(unsigned char*) *buffer);
-  printf("deserializzo %d bytes\n",*size);
-  //sensordata2=ninux__sensordata__unpack(NULL,*size,buffer);/// FUNZIONA
-  sensordata_deserialize2_size(psensordata2, buffer,size);
-  printf("size:%d\n",*size);
-  Ninux__Myset *set3;
-  set3= malloc (sizeof (Ninux__Myset)); //one insert at once
-  sensordata_add_set(sensordata2,set3,timestamp);
-  int i;
-  for(i=0; i<len;i++){
-  	sensordata_add_entry(sensordata2,set3,keys[i],values[i]);
-  }
-  sensordata_print_all(sensordata2);
-  //sensordata_serialize(sensordata2,(uint8_t**) buffer);
-  printf("size buf:%d\n",*size);
-  sensordata_serialize2_size(sensordata2, buffer,size);
-  //printf("mio buffer: %s\n",*buffer);
-  //printf("lunghezza buf:%d\n",strlen((char*)*buffer));
-  /////////////
-  return 0;
-}
 int test4(){
 
   // inizializzazione
   Ninux__Sensordata sensordata = NINUX__SENSORDATA__INIT;
   sensordata_init(&sensordata);
-  char* buffer;
+  //char* buffer;
   sensordata_serialize2_size(&sensordata,(uint8_t**) rtc_buffer,&rtc_buffer_len);
   ///////
 
 
-  //printf("mio buffer: %s\n",buffer);
-  //printf("lunghezza buf:%d\n",strlen((char*)buffer));
 
-  	//insert_values(&buffer);
-        char* keys[]={"temp","hum","wind","pres"}; 
-        int values[]={22,78,23,1013};
-  	insert_values2(&rtc_buffer,12334234,keys,values,4,&rtc_buffer_len);
-	//keys[0]="temp";
-        //values[0]=33;
-        printf("fine primo inserimento\n");
-        printf("%s\n",rtc_buffer);
-        char* keys2[]={"temp","hum","wind","pres"}; 
-        int values2[]={25,80,35,999};
-  	insert_values2(&rtc_buffer,4444444,keys2,values2,4,&rtc_buffer_len);
-        char* keys3[]={"temp","hum","wind","pres"}; 
-        int values3[]={250,800,350,9999};
-  	insert_values2(&rtc_buffer,5555555,keys3,values3,4,&rtc_buffer_len);
+  //insert_values(&buffer);
+  char* keys[]={"temp","hum","wind","pres"}; 
+  int values[]={22,78,23,1013};
+  sensordata_insert_values2(&rtc_buffer,12334234,keys,values,4,&rtc_buffer_len);
 
-  ///// inserimento singolo a rotazione
-  //Ninux__Sensordata *sensordata2;
-  //Ninux__Sensordata *psensordata2 = &sensordata2;
-  //sensordata_deserialize(psensordata2,(unsigned char*) buffer);
-  //Ninux__Myset *set3;
-  //set3= malloc (sizeof (Ninux__Myset)); //one insert at once
-  //sensordata_add_set(sensordata2,set3,555555);
-  //sensordata_add_entry(sensordata2,set3,"temp",55);
-  //sensordata_add_entry(sensordata2,set3,"pres",5555);
-  //sensordata_print_all(sensordata2);
-  //sensordata_serialize(sensordata2,(uint8_t**) &buffer);
-  //printf("mio buffer: %s\n",buffer);
-  //printf("lunghezza buf:%d\n",strlen((char*)buffer));
-  ///////////////
+  //keys[0]="temp";
+  //values[0]=33;
+  printf("fine primo inserimento\n");
+  printf("%s\n",rtc_buffer);
 
+  char* keys2[]={"temp","hum","wind","pres"}; 
+  int values2[]={25,80,35,999};
+  sensordata_insert_values2(&rtc_buffer,4444444,keys2,values2,4,&rtc_buffer_len);
+
+  char* keys3[]={"temp","hum","wind","pres"}; 
+  int values3[]={250,800,350,9999};
+  sensordata_insert_values2(&rtc_buffer,5555555,keys3,values3,4,&rtc_buffer_len);
+
+  char* keys4[]={"temp","batt"}; 
+  int values4[]={250,42};
+  sensordata_insert_values2(&rtc_buffer,666666,keys4,values4,2,&rtc_buffer_len);
   
+
   //sensordata_free(&sensordata);
   //sensordata_free(&sensordata2);
   return 0;
